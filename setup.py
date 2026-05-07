f86f702a58be3f2d7994b2643438e5cadc5e4a69
TashfiaS <reeti.tashfia@gmail.com>
2026-05-07 06:17:09 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Tashfia",
+    author_email="reeti.tashfia@gmail.com",
+    packages=find_packages()
+)
