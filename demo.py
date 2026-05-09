9c9952888192c2b106428c3a72f905d13af69e30
TashfiaS <reeti.tashfia@gmail.com>
2026-05-10 03:35:22 +0600

    Added logger, exception, and utils

---
+++ demo.py
@@ -0,0 +1,13 @@
+from us_visa.logger import logging
+from us_visa.exception import USVisaException
+import sys
+
+
+#logging.info("Logging set up complete. ")
+
+try:
+    r = 3/0
+    print(r)
+except Exception as e:
+    logging.info(e)
+    raise USVisaException(e, sys) from e
